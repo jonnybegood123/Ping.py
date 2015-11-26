9b6ad2f18d92864b9d0a7d8ba6160e6ff99214b5
Jonathan Robins <jonnybegood123@users.noreply.github.com>
2015-11-25 19:21:57 -0500

    proper timeouts

--- pingServer.py
+++ pingServer.py
@@ -4,13 +4,17 @@ import time
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(("", 1337))
+server.settimeout(10)
 
 while True:
 	message = ""
-	message, address = server.recvfrom(1337)
-	if message == "done":
-		sys.exit()		
-	if message != "":
-		time.sleep(2)
-		print("Server: Received message " + str(message) + " and sending back to client.")
-		server.sendto(str(message), address)
+	try:
+		message, address = server.recvfrom(1337)
+		if message == "done":
+			sys.exit()
+		if message != "":
+			print("Server: Received message " + str(message) + " and sending back to client.")
+			server.sendto(str(message), address)
+	except socket.timeout:
+		print "Server timed out due to not receiving any messages."
+		sys.exit()
