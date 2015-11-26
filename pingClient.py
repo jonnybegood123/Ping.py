aeb07c8c3e4c1e0a440e89d1f71de67e76d9c766
Jonathan Robins <jonnybegood123@users.noreply.github.com>
2015-11-25 19:22:29 -0500

    proper timeouts

--- pingClient.py
+++ pingClient.py
@@ -15,19 +15,22 @@ data = "DATA"
 #	sys.exit()
 
 sendingSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sendingSocket.settimeout(5)
+sendingSocket.settimeout(1)
 
 i = 1
 while i <= 10:
-	start = time.time() 
+	start = time.time()
 	sendingSocket.sendto(data + " " + str(i), (UDP_IP, UDP_PORT))
 	print "Client: Sending data containing contents: " + data + " " + str(i)
+	try:
+		returnedData, server = sendingSocket.recvfrom(1337)
+		print("Client: Received back message: " + returnedData)
+		rtt = time.time() - start
+		print("Elapsed time in seconds = " + str(rtt) + " seconds.")
+	except socket.timeout:
+		print "Packet containing containing contents: " + data + " " + str(i) + " timed out."
 	i = i+1
-	returnedData, server = sendingSocket.recvfrom(1337)
-	print("Client: Received back message: " + returnedData)
-	rtt = time.time() - start
-	print("Elapsed time in seconds = " + str(rtt) + " seconds.")
-	time.sleep(0.5)
+	time.sleep(2)
 print("Done!")
 
 sendingSocket.sendto("done", (UDP_IP,UDP_PORT))
